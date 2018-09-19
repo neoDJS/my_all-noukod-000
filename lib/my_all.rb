da633e601dd0f923a94a38d500bb675b67f2e5bc
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-09-19 20:28:18 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,9 @@
 require 'pry'
 
 def my_all?(collection)
-
+  block_return_values = []
+  collection.length.times do |i|
+    block_return_values << yield(collection[i])
+  end
+  !block_return_values.include?(false)
 end
